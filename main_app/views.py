6f7fdfe460ac034c828a0da74216ab1f50db59ef
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-03-07 16:21:24 -0600

    views configured, created view for home.  configured paths and got home.html displaying text

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Budget, Expense
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+
+# Home
+class Home(TemplateView):
+    template_name="home.html"
+
+# Budget List
+class BudgetList(TemplateView):
+    template_name="budget_list.html"
