653b4e9fc9de999e930c856c0f91a0789299822d
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-03-07 16:30:41 -0600

    added path and view for budget list page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('budgets/', views.BudgetList.as_view(), name='budget_list'),
 ]
